c8de1b0650dd8a86e62c8832fb2421aab437dfdc
Snehal Shelke <snehalshelke@pf9s-MacBook-Air-3.local>
2025-03-07 10:21:34 +0530

    bundle creation using docker

--- scripts/download.sh
+++ scripts/download.sh
@@ -4,3 +4,6 @@ mv *socat* socat.deb
 mv *ebtables* ebtables.deb
 mv *ethtool* ethtool.deb
 mv *conntrack* conntrack.deb
+cp /root/pf9-byohost-agent.deb .
+cd ..
+imgpkg push -f dep/ -i snhpf9/byoh-bundle:v1.0
