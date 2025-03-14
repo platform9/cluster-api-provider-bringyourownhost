43c858ce5bd6d1e3479a6ab4ec493efdbef1d152
Snehal Shelke <snehalshelke@pf9s-MacBook-Air-3.local>
2025-03-14 10:00:53 +0530

    cleaned branch

--- build/pf9-byohost/dependencies/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "setting up kernal modules inside container"
-apt-get update && apt-get install -y linux-modules-$(uname -r)
-echo " Starting pf9-byohost-agent"
-hostnamectl set-hostname $HOSTNAME 
-dpkg -i pf9-byohost-agent.deb
-systemctl daemon-reload
-systemctl enable pf9-byohost-agent.service
-systemctl start pf9-byohost-agent.service
