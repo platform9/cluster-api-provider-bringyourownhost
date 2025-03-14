cef79a27ad0820de1d2c761cbaee1edeeff7ac5e
Snehal Shelke <snehalshelke@pf9s-MacBook-Air-3.local>
2025-03-14 21:14:51 +0530

    add byoh-image

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,32 +1,20 @@
-command -v imgpkg >/dev/null 2>&1
-IMGPKG=$?
-if [ "$IMGPKG" -ne 0 ]; then
-	echo "imgpkg not present"
-	echo "installing imgpkg"
-	sudo apt update && sudo apt install -y imgpkg
-fi
-echo "creating /var/lib/pf9/ dir"
-mkdir -p /var/lib/pf9/
-cd /var/lib/pf9/
-echo "Pulling Platform9 Byohost Packages"
-if imgpkg pull -i quay.io/platform9/pf9-byoh:byoh-agent --output /var/lib/pf9/; then 
-	echo "Successfully Pulled Byohost Packages"
-	echo "installing socat"
-	dpkg -i socat.deb
-	echo "installing ethtool"
-	dpkg -i ethtool.deb
-	echo "installing ebtables"
-	dpkg -i ebtables.deb
-	echo "installing conntrack"
-	dpkg -i conntrack.deb
-	echo "installing pf9-byohost-agent"
-	dpkg -i pf9-byohost-agent.deb 
-	echo "Starting Systemd service for Byohost Agent"
-	systemctl daemon-reload
-	systemctl enable pf9-byohost-agent.service
-	echo "pf9-byohost-agent systemd service enabled successfully"
-	systemctl start pf9-byohost-agent.service
-	echo "pf9-byohost-agent systemd service started successfully"
-else 
-	echo "Failed to Pull PF9 Byohost Packages  , please check network connection and restart byoctl "
-fi
+#!/bin/bash
+echo "setting up kernal modules inside container"
+echo "setting up kernel modules inside container"
+	apt-get update
+	KERNEL_VERSION=$(uname -r)
+	if apt-cache search linux-modules-$KERNEL_VERSION | grep -q linux-modules-$KERNEL_VERSION; then
+	  apt-get install -y linux-modules-$KERNEL_VERSION
+	else
+	  echo "Warning: linux-modules-$KERNEL_VERSION package not found. Continuing without installing kernel modules."
+	fi
+
+echo " Starting pf9-byohost-agent"
+hostnamectl set-hostname $HOSTNAME
+if ! dpkg -i pf9-byohost-agent.deb; then
+	    echo "Failed to install pf9-byohost-agent package"
+	    exit 1
+	fi
+systemctl daemon-reload
+systemctl enable pf9-byohost-agent.service
+systemctl start pf9-byohost-agent.service
