426a7a96cb92bfd22d1b7fd62a011f373da05fcb
Shubham Mali <shubham@platform9.com>
2025-03-20 23:36:44 +0530

    fixed bundle 4

--- .ci/build-push-bundle.sh
+++ .ci/build-push-bundle.sh
@@ -7,6 +7,7 @@ export BUILD_ONLY=${BUILD_ONLY:-1}
 export CONTAINERD_VERSION=${CONTAINERD_VERSION:-1.7.26}
 export KUBERNETES_VERSION=${KUBERNETES_VERSION:-1.31.0-1.1}
 export KUBERNETES_MAJOR_VERSION=${KUBERNETES_MAJOR_VERSION:-v1.31}
+export BUNDLE_VERSION=${BUNDLE_VERSION:-v1.31.0}
 export ARCH=${ARCH:-amd64}
 # export CNI_VERSION=${CNI_VERSION:-1.4.0-1.1}
 
@@ -34,5 +35,5 @@ docker cp byoh-bundle-container:/bundle ./bundle
 
 echo "pushing oci bundle to quay.io/platform9/byoh-bundle-ubuntu_20.04.1_x86-64_k8s"
 cd bundle
-./imgpkg push -f . -i quay.io/platform9/byoh-bundle-ubuntu_20.04.1_x86-64_k8s:$KUBERNETES_MAJOR_VERSION
+./imgpkg push -f . -i quay.io/platform9/byoh-bundle-ubuntu_20.04.1_x86-64_k8s:$BUNDLE_VERSION
 
