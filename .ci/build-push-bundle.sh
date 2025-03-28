72c7c971de7d2fdd700352522655e403c933e73e
Snehal Shelke <155723346+snslk@users.noreply.github.com>
2025-03-28 10:29:42 +0530

    Add cri-tools version env variable  (#22)
    * added env var for cri-tools version
    
    Co-authored-by: Snehal Shelke <snehalshelke@pf9s-MacBook-Air-3.local>

--- .ci/build-push-bundle.sh
+++ .ci/build-push-bundle.sh
@@ -9,7 +9,7 @@ export KUBERNETES_VERSION=${KUBERNETES_VERSION:-1.31.0-1.1}
 export KUBERNETES_MAJOR_VERSION=${KUBERNETES_MAJOR_VERSION:-v1.31}
 export BUNDLE_VERSION=${BUNDLE_VERSION:-v1.31.0}
 export ARCH=${ARCH:-amd64}
-# export CNI_VERSION=${CNI_VERSION:-1.4.0-1.1}
+export CRITOOL_VERSION=${CRITOOL_VERSION:-1.31.0-1.1}
 
 #alias shasum="sha512sum"
 echo "installing imgpkg"
@@ -24,7 +24,7 @@ docker build -t byoh-bundle .
 docker rm -f byoh-bundle-container
 
 echo "executing docker image"
-docker run -e BUILD_ONLY -e CONTAINERD_VERSION -e KUBERNETES_VERSION -e KUBERNETES_MAJOR_VERSION -e ARCH --name byoh-bundle-container -i byoh-bundle /bin/bash
+docker run -e CRITOOL_VERSION -e BUILD_ONLY -e CONTAINERD_VERSION -e KUBERNETES_VERSION -e KUBERNETES_MAJOR_VERSION -e ARCH --name byoh-bundle-container -i byoh-bundle /bin/bash
 
 echo "creating bundle dir to push k8s packages"
 mkdir -p ./bundle
@@ -34,4 +34,3 @@ docker cp byoh-bundle-container:/bundle/. ./bundle/
 
 echo "pushing oci bundle to quay.io/platform9/byoh-bundle-ubuntu_20.04.1_x86-64_k8s"
 ./imgpkg push -f ./bundle -i quay.io/platform9/byoh-bundle-ubuntu_20.04.1_x86-64_k8s:$BUNDLE_VERSION
-
