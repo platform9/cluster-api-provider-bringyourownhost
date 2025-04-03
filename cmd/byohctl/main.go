db07e660e2880f7782caa320ebd60de62d068ae3
Jayanth Reddy <jaya@platform9.com>
2025-04-03 15:32:49 +0530

    Makefile for byohctl

--- cmd/byohctl/main.go
+++ cmd/byohctl/main.go
@@ -2,14 +2,32 @@
 package main
 
 import (
-    "os"
-    "github.com/platform9/cluster-api-provider-bringyourownhost/cmd/byohctl/cmd"
-    "github.com/platform9/cluster-api-provider-bringyourownhost/cmd/byohctl/utils"
+	"os"
+
+	"github.com/platform9/cluster-api-provider-bringyourownhost/cmd/byohctl/cmd"
+	"github.com/platform9/cluster-api-provider-bringyourownhost/cmd/byohctl/utils"
+	"github.com/platform9/cluster-api-provider-bringyourownhost/cmd/byohctl/version"
 )
 
+// Version information set by linker flags
+var (
+	buildVersion string
+	buildCommit  string
+)
+
+func init() {
+	// Set version information
+	if buildVersion != "" {
+		version.Version = buildVersion
+	}
+	if buildCommit != "" {
+		version.GitCommit = buildCommit
+	}
+}
+
 func main() {
-    if err := cmd.Execute(); err != nil {
-        utils.LogError("Command execution failed: %s", err.Error())
-        os.Exit(1)
-    }
+	if err := cmd.Execute(); err != nil {
+		utils.LogError("Command execution failed: %s", err.Error())
+		os.Exit(1)
+	}
 }
