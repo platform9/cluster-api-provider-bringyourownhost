5d042ba75bda62d19d3796633c88f10afe71f668
Harsh Srivastava <harshsrivastava@C02G82GRML7H.local>
2025-07-11 15:13:37 +0530

    Version Bump: Updated version of ubuntu from 20.04 to 22.04

--- controllers/infrastructure/k8sinstallerconfig_controller.go
+++ controllers/infrastructure/k8sinstallerconfig_controller.go
@@ -254,7 +254,7 @@ func (r *K8sInstallerConfigReconciler) storeInstallationData(ctx context.Context
 	scope.Config.Status.Ready = true
 	logger.Info("created installation and uninstallation secrets")
 
-	// ✅ Persist the status update
+	// Persist the status update
 	if err := r.Status().Update(ctx, scope.Config); err != nil {
 		return errors.Wrapf(err, "failed to update K8sInstallerConfig status with installation/uninstallation secret references")
 	}
