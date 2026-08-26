bbb9db8b7bd215e90a8b068b5692e42e69c22026
Snehal Shelke <snehal@platform9.com>
2026-08-26 12:05:54 +0530

    fix decommissionCmd

--- cmd/byohctl/cmd/decommission.go
+++ cmd/byohctl/cmd/decommission.go
@@ -28,7 +28,7 @@ This command will:
 
 func init() {
 	rootCmd.AddCommand(decommissionCmd)
-	deauthoriseCmd.Flags().StringVarP(&verbosity, "verbosity", "v", "minimal", "Log verbosity level. Requires one of: all, important, minimal, critical.\nOmitting the flag will show minimal verbosity")
+	decommissionCmd.Flags().StringVarP(&verbosity, "verbosity", "v", "minimal", "Log verbosity level. Requires one of: all, important, minimal, critical.\nOmitting the flag will show minimal verbosity")
 }
 
 func runDecommission(cmd *cobra.Command, args []string) {
